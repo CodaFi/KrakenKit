cd2277e2c5580686f6bc044b51b94330dba0c7af
CodaFi <devteam.codafi@gmail.com>
2013-06-15 16:57:26 -0600

    Start JSON adapter

---
+++ src/cpp/JSONAdapter.h
@@ -0,0 +1,27 @@
+//
+//  JSONAdapter.h
+//  KrakenKit
+//
+//  Created by Robert Widmann on 6/15/13.
+//  Copyright (c) 2013 CodaFi. All rights reserved.
+//
+
+#ifndef __KrakenKit__JSONAdapter__
+#define __KrakenKit__JSONAdapter__
+
+#include <iostream>
+#include "Serializable_Object.h"
+
+
+namespace KrakenKit {
+	class JSONAdapter {
+	public:
+		Serializable_Object model();
+		static Serializable_Object modelFromJSONDictionary<class T>(std::map &JSON);
+		
+	private:
+		Serializable_Object mModel;
+	};
+}
+
+#endif /* defined(__KrakenKit__JSONAdapter__) */
